bd5c16ca376b325ead154b23bb8458df68a175b0
ahmed bakri <ahmedbakri2020@gmail.com>
2025-08-20 13:57:47 +0300

    add latest functions and new features

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -85,7 +85,7 @@ class WordController extends Controller
     /**
      * Show a word
      */
-    public function show(Request $request, Word $word)
+    public function show(UpdateWordRequest $request, Word $word)
     {
         $user = $request->user();
         return $this->successResponse(
