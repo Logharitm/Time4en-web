e9499fd093c1cae46bb70a543cfdf2d5a619f5c8
ahmed bakri <ahmedbakri2020@gmail.com>
2025-08-20 12:07:51 +0300

    add latest functions and new features

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Folder extends Model
 {
@@ -16,7 +17,7 @@ class Folder extends Model
         'description',
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
