ab1ec0f38450c91e837676a497153b85dbce9d1e
ahmed bakri <ahmedbakri2020@gmail.com>
2025-08-20 13:46:38 +0300

    add latest functions and new features

--- app/Http/Requests/WordShowRequest.php
+++ app/Http/Requests/WordShowRequest.php
@@ -10,14 +10,14 @@ class ShowWordRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        $user = $this->user();
-        $word = $this->route('word');
-
-        if ($user->role !== 'admin') {
-            if ($word->folder->user_id !== $user->id) {
-                throw new AuthorizationException('Unauthorized to view this word.');
-            }
-        }
+//        $user = $this->user();
+//        $word = $this->route('word');
+//
+//        if ($user->role !== 'admin') {
+//            if ($word->folder->user_id !== $user->id) {
+//                throw new AuthorizationException('Unauthorized to view this word.');
+//            }
+//        }
 
         return true;
     }
