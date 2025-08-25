7bf73601567cd44a284e7930ea2f438dfc13b15d
ahmed bakri <ahmedbakri2020@gmail.com>
2025-08-25 11:55:03 +0300

    add latest functions and new features

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -135,7 +135,7 @@ class WordController extends Controller
         $prompt = "Write a short meaningful sentence using the word: {$word}.";
 
         // استدعاء API
-        $response = Http::withToken('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')
+        $response = Http::withToken(env('HUGGINGFACE_API_KEY'))
             ->timeout(60)
             ->post('https://api-inference.huggingface.co/models/gpt2', [
                 'inputs' => $prompt,
