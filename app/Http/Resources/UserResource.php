2b898efc6c5d770d15dfcbbf31068cd29e34aee9
ahmed bakri <ahmedbakri2020@gmail.com>
2025-09-05 22:34:09 +0300

    Add new modules Users

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,7 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        dd($this->resource);
         return [
             'id' => $this->id,
             'name' => $this->name,
