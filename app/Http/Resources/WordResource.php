22c2b3359fadbd1631d05b1583787406d2b3935e
ahmed bakri <ahmedbakri2020@gmail.com>
2025-09-06 20:03:02 +0300

    Add new modules Users

--- app/Http/Resources/WordResource.php
+++ app/Http/Resources/WordResource.php
@@ -12,6 +12,9 @@ class WordResource extends JsonResource
         return [
             'id' => $this->id,
             'folder_id' => $this->folder_id,
+            'folder_name' => $this->folder->name,
+            'user_id' => $this->folder->user_id,
+            'user_name' => $this->folder->user->name,
             'word' => $this->word,
             'translation' => $this->translation,
             'example_sentence' => $this->example_sentence,
