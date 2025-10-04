740190ac7f51e47f8be113450c09673aab6dbcc1
ahmed bakri <ahmedbakri2020@gmail.com>
2025-10-04 21:23:33 +0300

    Add Notifications module

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -145,7 +145,7 @@ class NotificationController extends Controller
     function getAccessToken(): string
     {
         try {
-            $client = new Google\Client();
+            $client = new GoogleClient();
             $client->setAuthConfig(public_path('time4en-2cf44-163c8c65d3c2.json'));
             $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
 
